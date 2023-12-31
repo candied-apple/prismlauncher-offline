888d590e5a134e9e2fe89f142c26805464ad613f
candied apple <50497574+candied-apple@users.noreply.github.com>
2023-12-31 20:34:24 +0300

    offline support

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A custom launcher for Minecraft that allows you to easily manage multiple installations of Minecraft at once (Fork of MultiMC)";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nix-filter.url = "github:numtide/nix-filter";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs-stable.follows = "nixpkgs";
+      inputs.flake-compat.follows = "flake-compat";
+    };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+    libnbtplusplus = {
+      url = "github:PrismLauncher/libnbtplusplus";
+      flake = false;
+    };
+  };
+
+  outputs = {
+    flake-parts,
+    pre-commit-hooks,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        pre-commit-hooks.flakeModule
+
+        ./nix/dev.nix
+        ./nix/distribution.nix
+      ];
+
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+    };
+}
---
+++ launcher/ui/dialogs/ReviewMessageBox.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <QDialog>
+
+namespace Ui {
+class ReviewMessageBox;
+}
+
+class ReviewMessageBox : public QDialog {
+    Q_OBJECT
+
+   public:
+    static auto create(QWidget* parent, QString&& title, QString&& icon = "") -> ReviewMessageBox*;
+
+    using ResourceInformation = struct res_info {
+        QString name;
+        QString filename;
+        QString custom_file_path{};
+        QString provider;
+        QStringList required_by;
+        QString version_type;
+    };
+
+    void appendResource(ResourceInformation&& info);
+    auto deselectedResources() -> QStringList;
+
+    void retranslateUi(QString resources_name);
+
+    ~ReviewMessageBox() override;
+
+   protected:
+    ReviewMessageBox(QWidget* parent, const QString& title, const QString& icon);
+
+    Ui::ReviewMessageBox* ui;
+};
